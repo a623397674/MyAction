17a450d49119a840685eaab617c2a114b1b01086
GitHub Action <action@github.com>
2022-04-25 10:57:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/a623397674/myaction/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/a623397674/myaction/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.a623397674.myaction.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.a623397674.myaction.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/a623397674/myaction/services/MyProjectService.kt
+++ src/main/kotlin/com/github/a623397674/myaction/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.a623397674.myaction.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.a623397674.myaction.MyBundle
 
 class MyProjectService(project: Project) {
 
